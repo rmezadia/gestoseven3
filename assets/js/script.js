df4b76f2a39c59da54aaba0927243011cbfebb8f
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2022-11-10 16:28:45 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'horizontal',
+    captions: true,
+    slideWidth: 1600, // tamaño de slider
+    auto: true, // para que sea automatico
+    keyboardEnabled: true // para que puedas cambiar con
+  });
+});
